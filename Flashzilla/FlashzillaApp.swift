eb14e85a4410cd77226a0e165b24295e8c198b24
Daniel Copley <djcopley@users.noreply.github.com>
2023-01-24 00:06:56 -0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Daniel Copley on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
